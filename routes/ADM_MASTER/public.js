c1ded1662ef7e3d75d58f0b3fde1ce989a872e21
unknown <vinicius_23867@ETEC.FURLAN>
2025-09-04 08:49:10 -0300

    login ADM_Master

---
+++ routes/ADM_MASTER/public.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import supabase from '..supabase.js'
+import 'dotenv/config'
+
+const route = express.Router()
+
+route.post('login', async (req, res) => {
+    const { registration, password} = req.body
+
+    if (condition) {
+        
+    } else {
+        
+    }
+})
