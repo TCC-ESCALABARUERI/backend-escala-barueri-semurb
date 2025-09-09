4b67defda33290e38ee63ab0777b83854d0d3297
Vinicius Araujo <vinicius.jesus49@etec.sp.gov.br>
2025-09-08 21:09:54 -0300

    Rotas ADM Master (atualização)

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 import express from "express"
 import cors from 'cors'
-import dotenv from 'dotenv'
 
 //Importação das Rotas
-
+import loginMaster from './routes/ADM_MASTER/public.js'
+import routePrivateMaster from './routes/ADM_MASTER/private.js'
 
 const app = express()
-
 app.use(cors())
 app.use(express.json()) 
 
-dotenv.config()
+//Definição das Rotas
 
-console.log("Server is running on port 3000")
+app.use(
+    '/', 
+    // ADM Master
+    loginMaster,
+    routePrivateMaster
+    // ADM
+    // User
+)
 
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000')
+})
--- routes/ADM_MASTER/public.js
+++ routes/ADM_MASTER/public.js
@@ -1,15 +1,23 @@
 import express from 'express'
-import supabase from '..supabase.js'
-import 'dotenv/config'
+import dotenv from 'dotenv/config'
 
 const route = express.Router()
 
-route.post('login', async (req, res) => {
-    const { registration, password} = req.body
+route.post('/loginMaster', (req, res) => {
+    try {
+        const { registration, password } = req.body
 
-    if (condition) {
-        
-    } else {
-        
+        if (registration === process.env.REGISTRATION_MASTER && password === process.env.PASSWORD_MASTER) {
+            res.status(200).json({ message: 'Login bem-sucedido' })
+        }
+        else {
+            res.status(401).json({ error: 'Credenciais inválidas' })
+        }
+    }
+    catch (error) {
+        console.error('Erro ao fazer login:', error)
+        res.status(500).json({ error: 'Erro com o servidor' })
     }
 })
+
+export default route
