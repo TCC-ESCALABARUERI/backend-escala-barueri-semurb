3f7ef8b93b02dfbef33ec636d3ac3099e7a8214d
Vinicius Araujo <vinicius.jesus49@etec.sp.gov.br>
2025-09-19 02:01:18 -0300

    confirmação da escala

--- routes/EMPLOYEES/private.js
+++ routes/EMPLOYEES/private.js
@@ -5,5 +5,28 @@ import bcrypt from 'bcrypt'
 const route = express.Router()
 
 // confirmação de leitura da escala
+route.put('/confirmacaoEscala', async (req, res) => {
+    const { matricula_funcionario } = req.body
+    try {
+        //confirmação de leitura da escala 
+        const { data: confirmacao, error } = await supabase
+            .from('escala_confirmacao')
+            .update({
+                status: "Confirmado",
+                data_confirmacao: new Date().toISOString()
+            })
+            .eq('matricula_funcionario', matricula_funcionario)
+
+        if (error) {
+            throw error
+        }
+
+        res.status(200).json({ message: 'Escala confirmada com sucesso.' })
+    } catch (error) {
+        res.status(500).json({ message: 'Erro ao confirmar escala.', error: error.message })
+    }
+})
+
+// alteração de senha
 
 export default route
