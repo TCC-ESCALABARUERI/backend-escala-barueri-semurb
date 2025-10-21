b3b1c845195b94d389717895a29abe8295c38076
Vinicius Araujo <vinicius.jesus49@etec.sp.gov.br>
2025-10-21 07:41:18 -0300

    recuperacao de senha no login adm

--- routes/ADM/public.js
+++ routes/ADM/public.js
@@ -47,23 +47,183 @@ route.post('/loginAdm', async (req, res) => {
 
         // retornar escala do funcionario
         const { data: escala } = await supabase
-        .from('escala')
-        .select('*')
-        .eq('id_escala', funcionario.id_escala)
-        .maybeSingle()
+            .from('escala')
+            .select('*')
+            .eq('id_escala', funcionario.id_escala)
+            .maybeSingle()
 
         // retornar setor do funcionario
         const { data: setor } = await supabase
-        .from('setor')
-        .select('*')
-        .eq('id_setor', funcionario.id_setor)
-        .maybeSingle()
+            .from('setor')
+            .select('*')
+            .eq('id_setor', funcionario.id_setor)
+            .maybeSingle()
 
-        
+
         return res.status(200).json({ mensagem: 'Login bem-sucedido', funcionario, token, escala, setor })
     } catch (error) {
         return res.status(500).json({ mensagem: 'Erro no servidor', erro: error.message })
     }
 })
+
+// Envio de código de verificação por email para ADM
+route.post('/envioVerificacaoAdm_email', async (req, res) => {
+    try {
+        const obrigatorios = ['email', 'matricula_funcionario']
+        const campoFaltando = validarCampos(obrigatorios, req.body)
+        if (campoFaltando) return res.status(400).json({ mensagem: `Campo obrigatório ausente: ${campoFaltando}` })
+
+        const { email, matricula_funcionario } = req.body
+
+        const { data: funcionario } = await supabase
+            .from('funcionario')
+            .select('*')
+            .eq('matricula_funcionario', matricula_funcionario)
+            .maybeSingle()
+
+            if(!funcionario) {
+                return res.status(400).json({mensagem: 'Erro ao buscar usuário'})
+            }
+
+        const { data: emailFuncionario, error } = await supabase
+            .from('funcionario')
+            .select('*')
+            .eq('email', email)
+            .maybeSingle()
+
+        if (error) return res.status(400).json({ mensagem: 'Erro ao buscar email do funcionŕio', erro: error })
+        if (!funcionario) return res.status(404).json({ mensagem: 'Usuário não encontrado' })
+
+        // verificar se existe a permissao de adm
+        if (funcionario.status_permissao !== 'Sim') {
+            return res.status(403).json({ mensagem: 'Acesso negado: Permissão de administrador necessária' })
+        }
+
+        // Gerar um código de verificação (pode ser um número aleatório ou uma string)
+        const codigoVerificacao = Math.floor(100000 + Math.random() * 900000).toString() // Exemplo: código de 6 dígitos
+
+        await fetch('https://webmailsender.vercel.app', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                credentialType: 'oauth2',
+                credentials: {
+                    user: process.env.EMAIL_USER,
+                    clientId: process.env.EMAIL_CLIENT_ID,
+                    clientSecret: process.env.EMAIL_CLIENT_SECRET,
+                    refreshToken: process.env.REFRESH_TOKEN
+                },
+                mailOptions: {
+                    from: `"Escala Semurb" <${process.env.EMAIL_USER}>`,
+                    to: emailFuncionario.email,
+                    subject: 'Código de Verificação',
+                    html: `<h1 style="color: pink">Seu código de verificação é: ${codigoVerificacao}</h1>`
+                }
+            }),
+        })
+
+        // armazenar o codigo no banco de dados
+        const { novoCodigo } = await supabase
+            .from('codigo_validacao')
+            .insert([{
+                codigo: codigoVerificacao,
+                matricula_funcionario: matricula_funcionario
+            }])
+
+        res.status(200).json({ mensagem: 'Código de verificação enviado com sucesso', codigoVerificacao })
+    } catch (error) {
+        return res.status(500).json({ mensagem: 'Erro no servidor', erro: error.message })
+    }
+})
+
+// verificação do código enviado para ADM
+route.post('/verificacaoCodigoAdm', async (req, res) => {
+    try {
+        const obrigatorios = ['matricula_funcionario', 'codigo']
+        const campoFaltando = validarCampos(obrigatorios, req.body)
+        if (campoFaltando) return res.status(400).json({ mensagem: `Campo obrigatório ausente: ${campoFaltando}` })
+
+        const { matricula_funcionario, codigo } = req.body
+
+        const { data: codigoValidacao, error } = await supabase
+            .from('codigo_validacao')
+            .select('*')
+            .eq('matricula_funcionario', matricula_funcionario)
+            .eq('codigo', codigo)
+            .maybeSingle()
+
+        if (error) return res.status(400).json({ mensagem: 'Erro ao buscar código de verificação', erro: error })
+        
+            // Verificar se o código existe e é válido, e verificar se não expirou (apos 5 minutos)
+        if (!codigoValidacao) {
+            return res.status(400).json({ mensagem: 'Código de verificação inválido' })
+        }
+
+        const agora = new Date()
+        const criadoEm = new Date(codigoValidacao.criado_em)
+        const diferencaEmMinutos = (agora - criadoEm) / 1000 / 60
+
+        if (diferencaEmMinutos > 5) {
+            return res.status(400).json({ mensagem: 'Código de verificação expirado' })
+        }
+
+        return res.status(200).json({ mensagem: 'Código de verificação válido' })
+    } catch (error) {
+        return res.status(500).json({ mensagem: 'Erro no servidor', erro: error.message })
+    }
+})
+
+route.put('/redefinirSenhaAdm', async (req, res) => {
+    try {
+        const obrigatorios = ['codigo', 'matricula_funcionario', 'nova_senha', 'confirmar_senha']
+        const campoFaltando = validarCampos(obrigatorios, req.body)
+        if (campoFaltando) return res.status(400).json({ mensagem: `Campo obrigatório ausente: ${campoFaltando}` })
+            
+        const { codigo, matricula_funcionario, nova_senha, confirmar_senha } = req.body
+
+        const { data: codigoValidacao, error } = await supabase
+            .from('codigo_validacao')
+            .select('*')
+            .eq('matricula_funcionario', matricula_funcionario)
+            .eq('codigo', codigo)
+            .maybeSingle()
+
+        if (error) return res.status(400).json({ mensagem: 'Erro ao buscar código de verificação', erro: error })
+        if (!codigoValidacao) {
+            return res.status(400).json({ mensagem: 'Código de verificação inválido' })
+        }
+
+        //verificar se o código expirou (5 minutos)
+        const agora = new Date()
+        const criadoEm = new Date(codigoValidacao.criado_em)
+        const diferencaEmMinutos = (agora - criadoEm) / 1000 / 60
+
+        if (diferencaEmMinutos > 5) {
+            return res.status(400).json({ mensagem: 'Código de verificação expirado' })
+        }
+
+        if (nova_senha !== confirmar_senha) {
+            return res.status(400).json({ mensagem: 'As senhas não coincidem' })
+        }
+
+        const senhaHash = await bcrypt.hash(nova_senha, 10)
+
+        const { data: funcionarioAtualizado, error: erroAtualizacao } = await supabase
+            .from('funcionario')
+            .update({ senha: senhaHash })
+            .eq('matricula_funcionario', matricula_funcionario)
+            .select()
+            .maybeSingle()
+
+        if (erroAtualizacao) return res.status(400).json({ mensagem: 'Erro ao atualizar a senha', erro: erroAtualizacao })
+
+        return res.status(200).json({ mensagem: 'Senha atualizada com sucesso', funcionarioAtualizado })
+    }
+    catch (error) {
+        return res.status(500).json({ mensagem: 'Erro no servidor', erro: error.message })
+    }
+})
+
+
 
 export default route
